0351adf7210c8e2f3c241ef8760c9602999c752a
Minki Kim <alsrl0023@naver.com>
2022-10-01 17:01:53 +0900

    feat: page title 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>농산물 실시간/경락가격 정보</title>
+      </Head>
+      <Component {...pageProps} />)
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
