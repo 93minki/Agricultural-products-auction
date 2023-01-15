8e33babee89e50004c8742ae0fd560a980a4ca15
Minki Kim <alsrl0023@naver.com>
2023-01-15 21:06:32 +0900

    fix: API Loading 상태 추가

--- src/layout/SettlementPageLayout/SettlementPage.tsx
+++ src/layout/SettlementPageLayout/SettlementPage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Loading from "../../components/Loading/Loading";
 import RecentKeyword from "../../components/RecentKeyword/RecentKeyword";
 import SearchButton from "../../components/SearchButton/SearchButton";
 import SearchInfo from "../../components/SearchInfo/SerachInfo";
@@ -6,13 +8,14 @@ import Title from "../../components/Title/Title";
 import * as S from "./style";
 
 const SettlementPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <S.PageLayout>
       <Title title="정산 가격 정보" />
       <SearchInfo type="settlement" />
-      <SearchButton />
+      <SearchButton setIsLoading={setIsLoading} />
       <RecentKeyword />
-      <SettlementProductList />
+      {isLoading ? <Loading /> : <SettlementProductList />}
     </S.PageLayout>
   );
 };
